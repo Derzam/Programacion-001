1d17cd51ac1e78779bfe231fd162eacfef09bbe7
Derzam <derlymoreira192@gmail.com>
2021-08-03 19:54:37 -0500

    Taller 2

---
+++ TrabajosProgramacion/src/ejercicios/varios/vectores/matrices/MainVector1.java
@@ -0,0 +1,39 @@
+package ejercicios.varios.vectores.matrices;
+
+import java.util.Scanner;
+
+public class MainVector1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declare un vetor(3) de tipo char y construya un programa
+		//que pida al ususario que ingrese los valores para cada posicion
+		// y finalmente que se imprima el valor de todos los valores ingresados
+		
+		char X[]=new char[3];
+		
+		for(int i = 0; i<3;i++) {
+			System.out.println("Ingrese el valor de la posicion:" +i);
+			Scanner lector = new Scanner(System.in);
+			
+			char n = lector.next().charAt(0);
+			
+			X[i]=n;	
+		}
+			
+			//Recorriendo el vector lleno
+			
+			for(int i = 0; i<3;i++) {
+				System.out.println("El valor de la posicion:" +i);
+				System.out.println(X[i]);
+		
+
+			
+	
+	}
+}
+		
+	}
+
+ 
---
+++ TrabajosProgramacion/src/ejercicios/varios/vectores/matrices/MainVector2.java
@@ -0,0 +1,31 @@
+package ejercicios.varios.vectores.matrices;
+
+import java.util.Scanner;
+
+public class MainVector2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declare un vector X cuya longitud se pida al usuario
+		// y construya un programa que pida al usuario que
+		// ingrese los valores para cada posicion
+		// y finalmente que imprima todos los valores ingresados
+		
+		Scanner lector = new Scanner(System.in);
+		
+		System.out.println("Ingrese una longitud");
+		int n = lector.nextInt();
+
+		int X[] = new int[n];
+
+		for (int i = 0; i > n; i++) {
+			System.out.println("La longitud del vector es:"+i);
+			X[i]=n;	
+			
+		
+			
+
+		}
+
+	}
+}
