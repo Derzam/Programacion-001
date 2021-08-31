bba7a298d6f92111ffc8b5ebf7112ec56ec5a92e
Derzam <derlymoreira192@gmail.com>
2021-08-31 18:42:55 -0500

    Deber

--- TrabajosProgramacion/src/deber/diagrama/de/flujo/BancoPichincha.java
+++ TrabajosProgramacion/src/deber/diagrama/de/flujo/BancoPichincha.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BancoPichincha {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 
 		Scanner lector = new Scanner(System.in);
 
